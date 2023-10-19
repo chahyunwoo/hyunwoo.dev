0d9282a9c64e393ed52e61f1ce5d5df2d512a2a6
hyunwoo93.cha <hyunwoo93.cha@lgepartners.com>
2023-10-20 01:48:24 +0900

    모바일 헤더 높이 수정/TODO: allPosts 전역 상태 관리

--- components/MobileMenuList.tsx
+++ components/MobileMenuList.tsx
@@ -7,7 +7,7 @@ interface IProps {
 
 export default function MobileMenuList({ setMenuOpen }: IProps) {
   return (
-    <div className="fixed top-16 left-0 w-full h-full bg-neutral-100 dark:bg-neutral-900">
+    <div className="fixed top-14 md:top-16 left-0 w-full h-full bg-neutral-100 dark:bg-neutral-900">
       <ul className="flex flex-col gap-4 p-4">
         {navlinks.map(({ title, href }) => (
           <li
