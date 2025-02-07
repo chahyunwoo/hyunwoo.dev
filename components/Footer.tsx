8d7e88030cf76c73c6ad88970b2370368979ff2e
hyunwoo93.cha <hyunwoo93.cha@lgepartners.com>
2025-02-07 14:40:13 +0900

    footer text change

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className="w-full px-8 border-t flex justify-center items-center dark:border-neutral-600">
       <div className="w-full max-w-screen-sm flex justify-center items-center py-6">
         <p className="text-gray-400 dark:text-neutral-600 text-sm">
-          &copy; 2023. Cha Hyun Woo. All rights reserved.
+          &copy; 2025. Cha Hyun Woo. All rights reserved.
         </p>
       </div>
     </footer>
